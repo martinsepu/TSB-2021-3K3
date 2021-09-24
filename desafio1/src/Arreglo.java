f9bab475f40ab32e7df1d86f226a23b78c695c13
Martin Sepulveda <martin.esa2901@gmail.com>
2021-09-24 01:40:32 -0300

    primer subida

--- desafio1/src/Arreglo.java
+++ desafio1/src/Arreglo.java
@@ -1,6 +1,27 @@
 public class Arreglo {
     private int[] vector;
 
+    /**
+     * metodo que buscara el primer numero primo del arreglo
+     * @return: retornara el primer numero primo, si no existe ninguno retorna -1
+     */
+    public int buscarPrimerPrimo(){
+        boolean esPrimo=false;
+        for(int i=0;i<vector.length;i++){
+            if(vector[i]%2==0)
+                continue;
+            else{
+                for(int j=3;j<vector[i]/2;j+=2){
+                    if(vector[i]%j==0){
+                        break;
+                    }
+                }
+                if(esPrimo)
+                    return vector[i];
+            }
+        }
+        return -1;
+    }
 
 
 }
