6266d153e1e8f9b8d64328c2796131153dbae0d1
Martin Sepulveda <martin.esa2901@gmail.com>
2021-09-25 10:08:12 -0300

    primer subida

--- desafio1/src/Arreglo.java
+++ desafio1/src/Arreglo.java
@@ -1,10 +1,12 @@
 public class Arreglo {
     private int vector[];
+    private boolean estaOrdenado;
 
 
     public Arreglo(int cant){
         if(cant<=0) cant=5;
         vector =new int[cant];
+        estaOrdenado=false;
 
     }
 
@@ -26,16 +28,72 @@ public class Arreglo {
             else{
                 for(int j=3;j<vector[i]/2;j+=2){
                     if(vector[i]%j==0){
-                        break;
                         noesPrimo = true;
+                        break;
+
 
                     }
                 }
-                if(noesPrimo)
+                if(!noesPrimo)
                     return vector[i];
+                noesPrimo=false;
             }
         }
         return -1;
+    }
+    public void quick(int izq, int der){
+        int i = izq, j = der, y;
+        int x = vector[(izq + der) / 2];
+        do
+        {
+            while(vector[i]<x && i<der) { i++; }
+            while(x<vector[j] && j>izq) { j--; }
+            if(i <= j)
+            {
+                y = vector[i];
+                vector[i] = vector[j];
+                vector[j] = y;
+                i++;
+                j--;
+            }
+        }
+        while(i <= j);
+        if(izq < j) { quick( izq, j ); }
+        if(i < der) { quick( i, der ); }
+    }
+    public void ordenarArreglo(){
+
+        quick(0,vector.length-1);
+    }
+
+    public long calculoMediana(){
+        long mediana;
+        long suma;
+        if(!estaOrdenado) {
+            this.ordenarArreglo();
+            estaOrdenado = true;
+        }
+        suma=vector[5000000]+vector[5000001];
+        double prom=(double) suma/2;
+        prom=(double)Math.round(prom*100d)/100d;
+        System.out.println("el promedio es: "+prom);
+        System.out.println("promedio entero: " + Math.round(prom) );
+        mediana= Math.round(prom);
+        return mediana;
+    }
+
+    public int buscarMayor() {
+        int mayor;
+        if(!estaOrdenado) {
+            this.ordenarArreglo();
+            estaOrdenado = true;
+        }
+        return vector[vector.length-1];
+    }
+
+    public int getItem(int pos){
+        return vector[pos];
+
     }
 
 
--- desafio1/src/Principal.java
+++ desafio1/src/Principal.java
@@ -1,14 +1,105 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
-        Arreglo array=new Arreglo(5);
 
+        Arreglo array=new Arreglo(10000000);
+
+        boolean uno=false,dos=false,tres=false,cuatro=false,cinco=false,seis=false,siete=false,ocho=false;
+
+        //carga del arreglo
+        File f = new File("lote01.txt");
+        int i=0;
+        //long suma=0;
+
+        try{
+            Scanner miEscaner = new Scanner(f);
+
+            while (miEscaner.hasNextInt()) {
+                int numero = miEscaner.nextInt();
+                //System.out.println(numero);
+                array.setItemVector(numero,i);
+                i++;
+                //suma=suma+numero;
+                switch (numero) {
+                    case 2755145:
+                        uno = true;
+                        break;
+                    case 12873556:
+                        dos = true;
+                        break;
+                    case 9121687:
+                        tres = true;
+                        break;
+                    case 1013111:
+                        cuatro = true;
+                        break;
+                    case 882943:
+                        cinco = true;
+                        break;
+                    case 923810:
+                        seis = true;
+                        break;
+                    case 716253:
+                        siete = true;
+                        break;
+                    case 987235:
+                        ocho = true;
+                }
+
+            }
+        }
+        catch (FileNotFoundException e){
+          System.err.println("El archivo no existe");
+        }
+
+        StringBuffer res = new StringBuffer("binario de existencia: ");
+        if (uno)res.append("1");
+        else res.append("0");
+        if (dos)res.append("1");
+        else res.append("0");
+        if (tres)res.append("1");
+        else res.append("0");
+        if (cuatro)res.append("1");
+        else res.append("0");
+        if (cinco)res.append("1");
+        else res.append("0");
+        if (seis)res.append("1");
+        else res.append("0");
+        if (siete)res.append("1");
+        else res.append("0");
+        if (ocho)res.append("1");
+        else res.append("0");
+        System.out.println(res);
+
+
+        // arreglo de prueba
+        /*
         array.setItemVector(4,0);
         array.setItemVector(8,1);
         array.setItemVector(9,2);
-        array.setItemVector(5,3);
+        array.setItemVector(3,3);
         array.setItemVector(5,4);
+        suma=4+8+9+3+5;
+        */
 
-        System.out.println(array.buscarPrimerPrimo());
+        //  Calculo de promedio
+        /*
+        double prom= (double)suma/10000000;
+        prom=(double)Math.round(prom*100d)/100d;
+        System.out.println(prom);
+        System.out.println("promedio: " + Math.round(prom) );
+        */
+
+        //System.out.println(i);
+        array.calculoMediana();
+        System.out.println(array.getItem(9999999));
+        System.out.println("ya ordene");
+
+        //System.out.println("el primer primo es: " + array.buscarPrimerPrimo());
+
+
 
 
     }
